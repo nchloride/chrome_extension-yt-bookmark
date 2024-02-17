4ac7ca738203a5fac15731259d7cd917b6cc506f
nchloride <clankz39@gmail.com>
2024-02-18 00:39:30 +0800

    add added the functionality on twitter

--- distraction.js
+++ distraction.js
@@ -1,30 +1,42 @@
 
 
 
-
-const callback =  () => {
-  document
-    .querySelectorAll(".x1hc1fzr.x1unhpq9.x6o7n8i div.x1lliihq")
-    .forEach(async(div) => {
-      if (div.classList.length == 1) {
-        const {blocklist} = await chrome.storage.local.get("blocklist");
-        const filter = blocklist.split("\n").filter(e=> e!="")
-        filter.forEach(e=>{
-          if (div?.textContent.toLowerCase().includes(e)) {
-            div.style.display = "none";
-            console.log("has text");
+const main = (feed,post,site) =>{
+  try{
+    const callback =  () => {
+      document
+        .querySelectorAll(post)
+        .forEach(async(div) => {
+          console.log(div.classList.length);
+          if (div.classList.length == 1 || site ==="twitter") {
+            const {blocklist} = site === "facebook" ? await chrome.storage.local.get("blocklist") : {blocklist:"ad"};
+            const filter = blocklist.split("\n").filter(e=> e!="")
+            filter.forEach(e=>{
+              if (div?.textContent.toLowerCase().includes(e)) {
+                div.style.display = "none";
+                console.log("has text");
+              }
+            })
           }
-        })
-      }
-    });
-};
-const config = { attributes: true, childList: true, subtree: true };
+        });
+    };
+    const config = { attributes: true, childList: true, subtree: true };
+    
+    const targetNode = document.querySelector(feed);
+    
+    const observer = new MutationObserver(callback);
+    
+    observer.observe(targetNode, config);
+  }
+  catch{
+    main(feed,post,site);
+  }
+  
 
-const targetNode = document.querySelector(".x1hc1fzr.x1unhpq9.x6o7n8i");
-
-const observer = new MutationObserver(callback);
-
-observer.observe(targetNode, config);
 
 
+}  
+
+window.location.href.includes("facebook") ? main(".x1hc1fzr.x1unhpq9.x6o7n8i",".x1hc1fzr.x1unhpq9.x6o7n8i div.x1lliihq","facebook") : 
+    main(".css-175oi2r",".css-175oi2r .r-1jkjb","twitter");
 
